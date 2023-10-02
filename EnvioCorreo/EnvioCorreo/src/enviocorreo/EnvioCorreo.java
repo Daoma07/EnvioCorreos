8268e467bf4db7122216d07af39c3ea077e04bd1
Daniel Alameda <daoma222@gmail.com>
2023-10-01 23:08:24 -0700

    v5

---
+++ EnvioCorreo/EnvioCorreo/src/enviocorreo/EnvioCorreo.java
@@ -0,0 +1,10 @@
+package enviocorreo;
+
+public class EnvioCorreo {
+
+    public static void main(String[] args) {
+        EnvioCorreos mEnvioCorreos = new EnvioCorreos();
+        mEnvioCorreos.setVisible(true);
+    }
+    
+}
